f162bfda87c5b677b1ad1c9ba38150482a298eb2
Munyaradzi Gordon Muneka <gordonsnekaz@gmail.com>
2020-08-28 20:52:54 +0200

    Initial Commit

---
+++ StudentKit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StudentKit
+//
+//  Created by Munyaradzi Gordon Muneka on 28/8/2020.
+//  Copyright © 2020 Munyaradzi Gordon Muneka. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
